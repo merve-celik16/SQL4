1977b3414f6f7efd208c3138a5ff3388e83fb41e
Merve <mervedursunn34@gmail.com>
2024-11-09 00:50:04 +0300

    Add files via upload

---
+++ ödev9.sql
@@ -0,0 +1,15 @@
+SELECT city, country FROM city-- city tablosu ile country tablosununda bulunan city ve country isimlerini birlikte gösterir.
+
+    INNER JOIN country ON city.country_id = country.country_id;
+
+SELECT payment_id, first_name, last_name FROM customer -- customer ve payment tablosundan payment_id,fisrt_name ve last_name birlikte gösterir
+    
+  INNER JOIN payment ON customer.customer_id = payment.customer_id; 
+
+
+	SELECT rental_id, first_name, last_name FROM customer
+    
+  INNER JOIN rental ON customer.customer_id = rental.customer_id; -- customer ve rental tablosundan payment_id,fisrt_name ve last_name birlikte gösterir
+    
+
+    
